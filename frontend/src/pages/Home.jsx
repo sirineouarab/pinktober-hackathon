55c38711a971e67f6ab6c562c7498034ab13223c
sirineouarab <sirineouarab@gmail.com>
2023-10-14 15:45:39 +0100

    changed bg

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,25 +20,26 @@ function Home() {
         <img src="woman.png"></img>
       </main>
       <section>
-        <h2>Our Features</h2>
-        <div>
-          <span> Inspiring Survivor Stories : </span>Explore heartfelt
-          testimonials from brave women who have faced breast cancer head-on.
-          Their stories of resilience and hope are a testament to the power of
-          the human spirit.
-        </div>
+        <h2>OUR FEATURES</h2>
+        <div className="div">
+          <div className="features">
+            <div>
+              <img src="pict1.png"></img>
+              <br></br>
+              <span> Inspiring Survivor Stories </span>
+            </div>
 
-        <div>
-          <span>Educational Resources :</span> Access a wealth of articles,
-          guides, and resources about breast cancer, its risk factors, screening
-          methods, and treatment options. Knowledge is your best defense against
-          this disease.
-        </div>
+            <div>
+              <img src="pict2.png"></img> <br></br>
+              <span>Educational Resources </span>
+            </div>
 
-        <div>
-          <span>Connect with Our Chatbot : </span>Have questions about breast
-          cancer? Our chatbot is here 24/7 to provide answers, offer support,
-          and guide you through your journey.
+            <div>
+              <img src="pict3.png"></img>
+              <br></br>
+              <span>Connect with Our Chatbot </span>
+            </div>
+          </div>
         </div>
       </section>
       <Footer />
